6abfe31fa00aecc8b38e2bcdb1c886712a821a37
glamdrag <gla.dragan@gmail.com>
2023-10-15 18:16:32 +0200

    First commit of ems-backend

---
+++ src/main/java/com/draganglamocic/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,23 @@
+package com.draganglamocic.ems.mapper;
+
+import com.draganglamocic.ems.dto.EmployeeDto;
+import com.draganglamocic.ems.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
