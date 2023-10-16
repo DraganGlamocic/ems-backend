1efd3981dcf6c9af2dfd97e98cde7fe7fdc0ff0a
glamdrag <gla.dragan@gmail.com>
2023-10-16 18:38:40 +0200

    Created getAllEmployees. Now able to GET list of all employees from database with Postman, and view list of all employees in MySQL Workbench.

--- src/main/java/com/draganglamocic/ems/service/EmployeeService.java
+++ src/main/java/com/draganglamocic/ems/service/EmployeeService.java
@@ -2,9 +2,13 @@ package com.draganglamocic.ems.service;
 
 import com.draganglamocic.ems.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 }
 
