9fca192111af64ec81f24470e0f984e69a627f20
glamdrag <gla.dragan@gmail.com>
2023-10-18 15:56:54 +0200

    Created deleteEmployee. Now able to DELETE employee by id from database with Postman

--- src/main/java/com/draganglamocic/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/draganglamocic/ems/service/impl/EmployeeServiceImpl.java
@@ -40,4 +40,30 @@ public class EmployeeServiceImpl implements EmployeeService{
                 .collect(Collectors.toList());
 
     }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee with id [" + employeeId + "] does not exist.")
+        );
+
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+
+        Employee updatedEmployeeObj = employeeRepository.save(employee);
+
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee with id [" + employeeId + "] does not exist.")
+        );
+
+        employeeRepository.deleteById(employeeId);
+
+    }
 }
